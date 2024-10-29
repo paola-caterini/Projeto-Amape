695e507a276dc7864c3823e73bc9b1e35fee5308
EmersonOsantos <emerson.o@ufms.br>
2024-10-29 19:34:53 -0400

    atualizando moradores e relatorios

--- sistemaAulas/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Diretório atual
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Caminho completo para o banco de dados
-DB_PATH = os.path.join(current_dir, 'sistema_aulas.db')
